6c0c3dcbff43c7248f5872c3ab001caf76f06db2
oscar <oscar.fitinghoff@gmail.com>
2018-02-04 12:38:27 +0100

    Removed old code

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Standings from '@/components/Standings'
 import AddGame from '@/components/AddGame'
 
@@ -10,18 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Add Game',
+      component: AddGame
     },
     {
       path: '/standings',
       name: 'Standings',
       component: Standings
-    },
-    {
-      path: '/addGame',
-      name: 'Add Game',
-      component: AddGame
     }
   ]
 })
